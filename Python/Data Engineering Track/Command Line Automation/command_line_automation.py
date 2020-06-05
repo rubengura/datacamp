788476402bfe9234487cd2d54dd1f03f556d510d
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-06-05 23:47:37 +0200

    Added continuation of Command Line Automation Course.

--- Python/Data Engineering Track/Command Line Automation/command_line_automation.py
+++ Python/Data Engineering Track/Command Line Automation/command_line_automation.py
@@ -10,3 +10,10 @@ ls = !ls
 
 # Capturing .csv filenames from a directory
 ls = !ls test_dir/*.csv
+
+# Unix pipes
+# Counting size of python files
+ls -l | awk '{ SUM +=$5} END {print SUM}'
+
+# Pipe multiple outputs using Pipe operators
+ls -l | grep .py | awk '{ SUM +=$5} END {print SUM}'
