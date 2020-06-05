a3913c0a27c83f4a7f64da4a9c42ce3308d0e550
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-06-05 23:42:13 +0200

    Added start of Command Line Automation Course.

---
+++ Python/Data Engineering Track/Command Line Automation/command_line_automation.py
@@ -0,0 +1,12 @@
+__author__ = "rgr"
+
+### LESSON 1
+# IPython commands
+# Showing free disk
+!df -h
+
+# Capturing output of a command
+ls = !ls
+
+# Capturing .csv filenames from a directory
+ls = !ls test_dir/*.csv
