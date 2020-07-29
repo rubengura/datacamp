7e73223a8b23d8be9519e52a5479d0b69fa73419
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-07-29 22:13:58 +0200

    Added A/B Testing in R course script (unfinished).

---
+++ R/A-B Testing/A-B_Testing.R
@@ -0,0 +1,55 @@
+# Title     : A/B Testing in R
+# Objective : Learn a beginning to end A/B Testing experiment
+# Created by: ruben
+# Created on: 29/07/2020
+
+# Load tidyverse
+library(tidyverse)
+library(powerMediation)
+
+# Read in data
+click_data <- read_csv("click_data.csv")
+click_data
+
+# Find oldest and most recent date
+min(click_data$visit_date)
+max(click_data$visit_date)
+
+# Calculate the mean conversion rate by day of the week
+click_data %>%
+  group_by(wday(visit_date)) %>%
+  summarize(conversion_rate = mean(clicked_adopt_today))
+
+# Calculate the mean conversion rate by week of the year
+click_data %>%
+  group_by(week(visit_date)) %>%
+  summarize(conversion_rate = mean(clicked_adopt_today))
+
+# Compute conversion rate by week of the year
+click_data_sum <- click_data %>%
+  group_by(week(visit_date)) %>%
+  summarize(conversion_rate = mean(clicked_adopt_today))
+
+# Build plot
+ggplot(click_data_sum, aes(x = `week(visit_date)`,
+                           y = conversion_rate)) +
+  geom_point() +
+  geom_line() +
+  scale_y_continuous(limits = c(0, 1),
+                     labels = percent)
+
+# Compute and look at sample size for experiment in August
+total_sample_size <- SSizeLogisticBin(p1 = 0.54,
+                                      p2 = 0.64,
+                                      B = 0.5,
+                                      alpha = 0.05,
+                                      power = 0.8)
+total_sample_size  # 758
+
+# Compute and look at sample size for experiment in August with a 5 percentage point increase
+total_sample_size <- SSizeLogisticBin(p1 = 0.54,
+                                      p2 = 0.59,
+                                      B = 0.5,
+                                      alpha = 0.05,
+                                      power = 0.8)
+total_sample_size  # 3085
