a0c26f4bdc8278d00a6d4d10584d11a9d6a91dbf
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-08-03 11:09:59 +0200

    Added Time Series Analysis SQL course script (lesson 2).

--- SQL/Time Series Analysis in SQL Server/Time_Series_Analysis.sql
+++ SQL/Time Series Analysis in SQL Server/Time_Series_Analysis.sql
@@ -1,3 +1,4 @@
+-- LESSON 1
 DECLARE
 	@SomeTime DATETIME2(7) = SYSUTCDATETIME();
 
@@ -246,3 +247,207 @@ WHERE
 	AND c.FiscalWeekOfYear > 30
     -- Only return weekends
 	AND c.IsWeekend = 1;
+
+
+-- LESSON 2
+-- Building dates from parts
+-- Create dates from component parts on the calendar table
+SELECT TOP(10)
+	DATEFROMPARTS(c.CalendarYear, c.CalendarMonth, c.Day) AS CalendarDate
+FROM dbo.Calendar c
+WHERE
+	c.CalendarYear = 2017
+ORDER BY
+	c.FiscalDayOfYear ASC;
+
+SELECT TOP(10)
+	c.CalendarQuarterName,
+	c.MonthName,
+	c.CalendarDayOfYear
+FROM dbo.Calendar c
+WHERE
+	-- Create dates from component parts
+	DATEFROMPARTS(c.CalendarYear, c.CalendarMonth, c.Day) >= '2018-06-01'
+	AND c.DayName = 'Tuesday'
+ORDER BY
+	c.FiscalYear,
+	c.FiscalDayOfYear ASC;
+
+
+-- Neil Armstrong and Buzz Aldrin landed the Apollo 11 Lunar Module
+-- (nicknamed The Eagle) on the moon on July 20th, 1969 at 20:17 UTC.
+-- They remained on the moon for approximately 21 1/2 hours,
+-- taking off on July 21st, 1969 at 18:54 UTC.
+
+SELECT
+	-- Mark the date and time the lunar module touched down
+    -- Use 24-hour notation for hours, so e.g., 9 PM is 21
+	DATETIME2FROMPARTS(1969, 07, 20, 20, 17, 00, 000, 0) AS TheEagleHasLanded,
+	-- Mark the date and time the lunar module took back off
+    -- Use 24-hour notation for hours, so e.g., 9 PM is 21
+	DATETIMEFROMPARTS(1969, 07, 21, 18, 54, 00, 000) AS MoonDeparture;
+
+-- January 19th, 2038 at 03:14:08 UTC
+
+SELECT
+	-- Fill in the millisecond PRIOR TO chaos
+	DATETIMEOFFSETFROMPARTS(2038, 01, 19, 03, 14, 07, 999, 0, 0, 3) AS LastMoment,
+    -- Fill in the date and time when we will experience the Y2.038K problem
+    -- Then convert to the Eastern Standard Time time zone
+	DATETIMEOFFSETFROMPARTS(2038, 01, 19, 03, 14, 08, 0, 0, 0, 3) AT TIME ZONE 'Eastern Standard Time' AS TimeForChaos;
+
+
+
+-- CAST(), FORMAT() and PARSE()
+SELECT
+	d.DateText AS String,
+	-- Cast as DATE
+	CAST(d.DateText AS DATE) AS StringAsDate,
+	-- Cast as DATETIME2(7)
+	CAST(d.DateText AS DATETIME2(7)) AS StringAsDateTime2
+FROM dbo.Dates d;
+
+
+SET LANGUAGE 'GERMAN'
+
+SELECT
+	d.DateText AS String,
+	-- Convert to DATE
+	CONVERT(DATE, d.DateText) AS StringAsDate,
+	-- Convert to DATETIME2(7)
+	CONVERT(DATETIME2(7), d.DateText) AS StringAsDateTime2
+FROM dbo.Dates d;
+
+SELECT
+	d.DateText AS String,
+	-- Parse as DATE using German
+	PARSE(d.DateText AS DATE USING 'de-DE') AS StringAsDate,
+	-- Parse as DATETIME2(7) using German
+	PARSE(d.DateText AS DATETIME2(7) USING 'de-DE') AS StringAsDateTime2
+FROM dbo.Dates d;
+
+
+
+-- Date Offsets
+DECLARE
+	@OlympicsUTC NVARCHAR(50) = N'2016-08-08 23:00:00';
+
+SELECT
+	-- Fill in the time zone for Brasilia, Brazil
+	SWITCHOFFSET(@OlympicsUTC, '-03:00') AS BrasiliaTime,
+	-- Fill in the time zone for Chicago, Illinois
+	SWITCHOFFSET(@OlympicsUTC, '-05:00') AS ChicagoTime,
+	-- Fill in the time zone for New Delhi, India
+	SWITCHOFFSET(@OlympicsUTC, '+05:30') AS NewDelhiTime;
+
+
+DECLARE
+	@OlympicsClosingUTC DATETIME2(0) = '2016-08-21 23:00:00';
+
+SELECT
+	-- Fill in 7 hours back and a -07:00 offset
+	TODATETIMEOFFSET(DATEADD(HOUR, -7, @OlympicsClosingUTC), '-07:00') AS PhoenixTime,
+	-- Fill in 12 hours forward and a +12:00 offset
+	TODATETIMEOFFSET(DATEADD(HOUR, 12, @OlympicsClosingUTC), '+12:00') AS TuvaluTime;
+
+
+-- Handling errors
+DECLARE
+	@GoodDateINTL NVARCHAR(30) = '2019-03-01 18:23:27.920',
+	@GoodDateDE NVARCHAR(30) = '13.4.2019',
+	@GoodDateUS NVARCHAR(30) = '4/13/2019',
+	@BadDate NVARCHAR(30) = N'SOME BAD DATE';
+
+-- The prior solution using TRY_CONVERT
+SELECT
+	TRY_CONVERT(DATETIME2(3), @GoodDateINTL) AS GoodDateINTL,
+	TRY_CONVERT(DATE, @GoodDateDE) AS GoodDateDE,
+	TRY_CONVERT(DATE, @GoodDateUS) AS GoodDateUS,
+	TRY_CONVERT(DATETIME2(3), @BadDate) AS BadDate;
+
+SELECT
+	-- Fill in the correct data type based on our input
+	TRY_CAST(@GoodDateINTL AS DATETIME2(3)) AS GoodDateINTL,
+    -- Be sure to match these data types with the
+    -- TRY_CONVERT() examples above!
+	TRY_CAST(@GoodDateDE AS DATE) AS GoodDateDE,
+	TRY_CAST(@GoodDateUS AS DATE) AS GoodDateUS,
+	TRY_CAST(@BadDate AS DATETIME2(3)) AS BadDate;
+
+SELECT
+	TRY_PARSE(@GoodDateINTL AS DATETIME2(3)) AS GoodDateINTL,
+    -- Fill in the correct region based on our input
+    -- Be sure to match these data types with the
+    -- TRY_CAST() examples above!
+	TRY_PARSE(@GoodDateDE AS DATE USING 'de-de') AS GoodDateDE,
+	TRY_PARSE(@GoodDateUS AS DATE USING 'en-us') AS GoodDateUS,
+    -- TRY_PARSE can't fix completely invalid dates
+	TRY_PARSE(@BadDate AS DATETIME2(3) USING 'sk-sk') AS BadDate;
+
+
+WITH EventDates AS
+(
+    SELECT
+        -- Fill in the missing try-conversion function
+        TRY_CONVERT(DATETIME2(3), it.EventDate) AT TIME ZONE it.TimeZone AS EventDateOffset,
+        it.TimeZone
+    FROM dbo.ImportedTime it
+        INNER JOIN sys.time_zone_info tzi
+			ON it.TimeZone = tzi.name
+)
+SELECT
+    -- Fill in the approppriate event date to convert
+	CONVERT(NVARCHAR(50), ed.EventDateOffset) AS EventDateOffsetString,
+	CONVERT(DATETIME2(0), ed.EventDateOffset) AS EventDateLocal,
+	ed.TimeZone,
+    -- Convert from a DATETIMEOFFSET to DATETIME at UTC
+	CAST(ed.EventDateOffset AT TIME ZONE 'UTC' AS DATETIME2(0)) AS EventDateUTC,
+    -- Convert from a DATETIMEOFFSET to DATETIME with time zone
+	CAST(ed.EventDateOffset AT TIME ZONE 'US Eastern Standard Time'  AS DATETIME2(0)) AS EventDateUSEast
+FROM EventDates ed;
+
+
+-- Try out how fast the TRY_CAST() function is
+-- by try-casting each DateText value to DATE
+DECLARE @StartTimeCast DATETIME2(7) = SYSUTCDATETIME();
+SELECT TRY_CAST(DateText AS DATE) AS TestDate FROM #DateText;
+DECLARE @EndTimeCast DATETIME2(7) = SYSUTCDATETIME();
+
+-- Determine how much time the conversion took by
+-- calculating the date difference from @StartTimeCast to @EndTimeCast
+SELECT
+    DATEDIFF(MILLISECOND, @StartTimeCast, @EndTimeCast) AS ExecutionTimeCast;
+
+-- ExecutionTimeCast
+--  17
+
+-- Try out how fast the TRY_CONVERT() function is
+-- by try-converting each DateText value to DATE
+DECLARE @StartTimeConvert DATETIME2(7) = SYSUTCDATETIME();
+SELECT TRY_CONVERT(DATE, DateText) AS TestDate FROM #DateText;
+DECLARE @EndTimeConvert DATETIME2(7) = SYSUTCDATETIME();
+
+-- Determine how much time the conversion took by
+-- calculating the difference from start time to end time
+SELECT
+    DATEDIFF(MILLISECOND, @StartTimeConvert, @EndTimeConvert) AS ExecutionTimeConvert;
+
+-- ExecutionTimeConvert
+-- 9
+
+-- Try out how fast the TRY_PARSE() function is
+-- by try-parsing each DateText value to DATE
+DECLARE @StartTimeParse DATETIME2(7) = SYSUTCDATETIME();
+SELECT TRY_PARSE(DateText AS DATE) AS TestDate FROM #DateText;
+DECLARE @EndTimeParse DATETIME2(7) = SYSUTCDATETIME();
+
+-- Determine how much time the conversion took by
+-- calculating the difference from start time to end time
+SELECT
+    DATEDIFF(MILLISECOND, @StartTimeParse, @EndTimeParse) AS ExecutionTimeParse;
+
+-- ExecutionTimeParse
+-- 529
+
+
+-- LESSON 3
