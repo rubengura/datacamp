87585a3946b892ba8048032a976b30b68d453300
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-08-04 12:53:58 +0200

    Added Functions for Manipulating Data in SQL Server course script (lesson 2).

--- SQL/Functions for Manipulating Data in SQL Server/Functions_for_Manipulating_Data.sql
+++ SQL/Functions for Manipulating Data in SQL Server/Functions_for_Manipulating_Data.sql
@@ -88,3 +88,215 @@ WHERE country = 'Belgium'
 	AND gender = 'F'
     -- Select only people who voted more than 20 times
     AND total_votes > 20;
+
+
+-- Manipulating Time
+-- Most common date function for retrieving current date:
+SELECT
+	SYSDATETIME() AS CurrentDate;
+
+-- Select the current date in UTC time (Universal Time Coordinate)
+-- using two different functions.
+SELECT
+	CURRENT_TIMESTAMP AS UTC_HighPrecision,
+	GETUTCDATE() AS UTC_LowPrecision;
+
+-- Select the local system's date, including the timezone information.
+SELECT
+	GETDATE() AS Timezone;
+
+-- Use two functions to query the system's local date, without timezone
+-- information. Show the dates in different formats.
+SELECT
+	CONVERT(VARCHAR(24), SYSDATETIME(), 107) AS HighPrecision,
+	CONVERT(VARCHAR(24), SYSDATETIME(), 102) AS LowPrecision;
+
+-- Use two functions to retrieve the current time, in Universal Time Coordinate.
+SELECT
+	CAST(SYSUTCDATETIME() AS time) AS HighPrecision,
+	CAST(SYSUTCDATETIME() AS time) AS LowPrecision;
+
+-- Extracting parts of a date
+SELECT
+	first_name,
+	last_name,
+	-- Extract the year of the first vote
+	YEAR(first_vote_date)  AS first_vote_year,
+    -- Extract the month of the first vote
+	MONTH(first_vote_date) AS first_vote_month,
+    -- Extract the day of the first vote
+	DAY(first_vote_date)   AS first_vote_day
+FROM voters
+-- The year of the first vote should be greater than 2015
+WHERE YEAR(first_vote_date) > 2015
+-- The day should not be the first day of the month
+  AND DAY(first_vote_date) <> 1;
+
+-- Generating descriptive date parts
+SELECT
+	first_name,
+	last_name,
+	first_vote_date,
+    -- Select the name of the month of the first vote
+	DATENAME(MONTH, first_vote_date) AS first_vote_month
+FROM voters;
+
+
+SELECT
+	first_name,
+	last_name,
+	first_vote_date,
+    -- Select the number of the day within the year
+	DATENAME(DAYOFYEAR, first_vote_date) AS first_vote_dayofyear
+FROM voters;
+
+SELECT
+	first_name,
+	last_name,
+	first_vote_date,
+    -- Select day of the week from the first vote date
+	DATENAME(WEEKDAY, first_vote_date) AS first_vote_dayofweek
+FROM voters;
+
+
+SELECT
+	first_name,
+	last_name,
+   	-- Extract the month number of the first vote
+	DATEPART(MONTH,first_vote_date) AS first_vote_month1,
+	-- Extract the month name of the first vote
+    DATENAME(MONTH,first_vote_date) AS first_vote_month2,
+	-- Extract the weekday number of the first vote
+	DATEPART(WEEKDAY,first_vote_date) AS first_vote_weekday1,
+    -- Extract the weekday name of the first vote
+	DATENAME(WEEKDAY,first_vote_date) AS first_vote_weekday2
+FROM voters;
+
+SELECT
+	first_name,
+	last_name,
+    -- Select the year of the first vote
+   	YEAR(first_vote_date) AS first_vote_year,
+    -- Select the month of the first vote
+	MONTH(first_vote_date) AS first_vote_month,
+    -- Create a date as the start of the month of the first vote
+	DATEFROMPARTS(YEAR(first_vote_date), MONTH(first_vote_date), 1) AS first_vote_starting_month
+FROM voters;
+
+
+-- Performing arithmetic operations on dates
+SELECT
+	first_name,
+	birthdate,
+    -- Add 18 years to the birthdate
+	DATEADD(YEAR, 18, birthdate) AS eighteenth_birthday
+  FROM voters;
+
+SELECT
+	first_name,
+	first_vote_date,
+    -- Add 5 days to the first voting date
+	DATEADD(DAY, 5, first_vote_date) AS processing_vote_date
+  FROM voters;
+
+SELECT
+	-- Subtract 476 days from the current date
+	DATEADD(DAY, -476, SYSDATETIME()) AS date_476days_ago;
+
+
+SELECT
+	first_name,
+	birthdate,
+	first_vote_date,
+    -- Select the diff between the 18th birthday and first vote
+	DATEDIFF(YEAR, DATEADD(YEAR, 18, birthdate), first_vote_date) AS adult_years_until_vote
+FROM voters;
+
+SELECT
+	-- Get the difference in weeks from 2019-01-01 until now
+	DATEDIFF(WEEK, '2019-01-01', SYSDATETIME()) AS weeks_passed;
+
+
+-- Validating if expression is a date
+DECLARE @date1 NVARCHAR(20) = '2018-30-12';
+
+-- Set the date format and check if the variable is a date
+SET DATEFORMAT ydm;
+SELECT ISDATE(@date1) AS result;
+
+
+DECLARE @date1 NVARCHAR(20) = '15/2019/4';
+
+-- Set the date format and check if the variable is a date
+SET DATEFORMAT dym;
+SELECT ISDATE(@date1) AS result;
+
+DECLARE @date1 NVARCHAR(20) = '10.13.2019';
+
+-- Set the date format and check if the variable is a date
+SET DATEFORMAT mdy;
+SELECT ISDATE(@date1) AS result;
+
+DECLARE @date1 NVARCHAR(20) = '18.4.2019';
+
+-- Set the date format and check if the variable is a date
+SET DATEFORMAT dmy;
+SELECT ISDATE(@date1) AS result;
+
+
+-- Language	 Date format
+-- English	    mdy
+-- Croatian	    ymd
+-- Dutch	    dmy
+
+DECLARE @date1 NVARCHAR(20) = '30.03.2019';
+
+-- Set the correct language
+SET LANGUAGE 'Dutch';
+SELECT
+	@date1 AS initial_date,
+    -- Check that the date is valid
+	ISDATE(@date1) AS is_valid,
+    -- Select the name of the month
+	DATENAME(MONTH, @date1) AS month_name;
+
+DECLARE @date1 NVARCHAR(20) = '32/12/13';
+
+-- Set the correct language
+SET LANGUAGE 'Croatian';
+SELECT
+	@date1 AS initial_date,
+    -- Check that the date is valid
+	ISDATE(@date1) AS is_valid,
+    -- Select the name of the month
+	DATENAME(MONTH, @date1) AS month_name,
+	-- Extract the year from the date
+	YEAR(@date1) AS year_name;
+
+DECLARE @date1 NVARCHAR(20) = '12/18/55';
+
+-- Set the correct language
+SET LANGUAGE English;
+SELECT
+	@date1 AS initial_date,
+    -- Check that the date is valid
+	ISDATE(@date1) AS is_valid,
+    -- Select the week day name
+	DATENAME(WEEKDAY, @date1) AS week_day,
+	-- Extract the year from the date
+	YEAR(@date1) AS year_name;
+
+SELECT
+	first_name,
+    last_name,
+    birthdate,
+	first_vote_date,
+	-- Find out on which day of the week each participant voted
+	DATENAME(weekday, first_vote_date) AS first_vote_weekday,
+	-- Find out the year of the first vote
+	YEAR(first_vote_date) AS first_vote_year,
+	-- Discover the participants' age when they joined the contest
+	DATEDIFF(YEAR, birthdate, first_vote_date) AS age_at_first_vote,
+	-- Calculate the current age of each voter
+	DATEDIFF(YEAR, birthdate, SYSDATETIME()) AS current_age
+FROM voters;
