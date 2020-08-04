baed29c19440ea088f5d5b909ec29972b45cd422
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-08-04 17:03:51 +0200

    Added Functions for Manipulating Data in SQL Server course script (lesson 3).

--- SQL/Functions for Manipulating Data in SQL Server/Functions_for_Manipulating_Data.sql
+++ SQL/Functions for Manipulating Data in SQL Server/Functions_for_Manipulating_Data.sql
@@ -1,3 +1,4 @@
+-- LESSON 1
 SELECT
 	company,
 	company_location,
@@ -90,6 +91,7 @@ WHERE country = 'Belgium'
     AND total_votes > 20;
 
 
+-- LESSON 2
 -- Manipulating Time
 -- Most common date function for retrieving current date:
 SELECT
@@ -300,3 +302,179 @@ SELECT
 	-- Calculate the current age of each voter
 	DATEDIFF(YEAR, birthdate, SYSDATETIME()) AS current_age
 FROM voters;
+
+-- LESSON 3
+-- Working with Strings
+SELECT TOP 10
+	company,
+	broad_bean_origin,
+	-- Calculate the length of the broad_bean_origin column
+	LEN(broad_bean_origin) AS length
+FROM ratings
+--Order the results based on the new column, descending
+ORDER BY length DESC;
+
+SELECT
+	first_name,
+	last_name,
+	email
+FROM voters
+-- Look for the "dan" expression in the first_name
+WHERE CHARINDEX('dan', first_name) > 0
+    -- Look for last_names that do not contain the letter "z"
+	AND CHARINDEX('z', last_name) = 0;
+
+SELECT
+	first_name,
+	last_name,
+	email
+FROM voters
+-- Look for first names that start with C and the 3rd letter is r
+WHERE PATINDEX('C_r%', first_name) > 0;
+
+
+SELECT
+	first_name,
+	last_name,
+	email
+FROM voters
+-- Look for first names that have an "a" followed by 0 or more letters and then have a "w"
+WHERE PATINDEX('%a%w%', first_name) > 0;
+
+SELECT
+	first_name,
+	last_name,
+	email
+FROM voters
+-- Look for first names that contain one of the letters: "x", "w", "q"
+WHERE PATINDEX('%[xwq]%', first_name) > 0;
+
+
+SELECT
+	company,
+	bean_type,
+	broad_bean_origin,
+    -- 'company' and 'broad_bean_origin' should be in uppercase
+	'The company ' +  UPPER(company) + ' uses beans of type "' + bean_type + '", originating from ' + UPPER(broad_bean_origin) + '.'
+FROM ratings
+WHERE
+    -- The 'broad_bean_origin' should not be unknown
+	LOWER(broad_bean_origin) NOT LIKE '%unknown%'
+     -- The 'bean_type' should not be unknown
+    AND LOWER(bean_type) NOT LIKE '%unknown%';
+
+
+SELECT
+	first_name,
+	last_name,
+	country,
+    -- Select only the first 3 characters from the first name
+	LEFT(first_name, 3) AS part1,
+    -- Select only the last 3 characters from the last name
+    RIGHT(last_name, 3) AS part2,
+    -- Select only the last 2 digits from the birth date
+    RIGHT(birthdate, 2) AS part3,
+    -- Create the alias for each voter
+    LEFT(first_name, 3) + RIGHT(last_name, 3) + '_' + RIGHT(birthdate, 2)
+FROM voters;
+
+
+SELECT
+	email,
+    -- Extract 5 characters from email, starting at position 3
+	SUBSTRING(email, 3, 5) AS some_letters
+FROM voters;
+
+DECLARE @sentence NVARCHAR(200) = 'Apples are neither oranges nor potatoes.'
+SELECT
+	-- Extract the word "Apples"
+	SUBSTRING(@sentence, 1, 6) AS fruit1,
+    -- Extract the word "oranges"
+	SUBSTRING(@sentence, 20, 7) AS fruit2;
+
+
+SELECT
+	first_name,
+	last_name,
+	email,
+	-- Replace "yahoo.com" with "live.com"
+	REPLACE(email, 'yahoo.com', 'live.com') AS new_email
+FROM voters;
+
+SELECT
+	company AS initial_name,
+    -- Replace '&' with 'and'
+	REPLACE(company, '&', 'and') AS new_name
+FROM ratings
+WHERE CHARINDEX('&', company) > 0;
+
+SELECT
+	company AS old_company,
+    -- Remove the text '(Valrhona)' from the name
+	REPLACE(company, '(Valrhona)', '') AS new_company,
+	bean_type,
+	broad_bean_origin
+FROM ratings
+WHERE company = 'La Maison du Chocolat (Valrhona)';
+
+-- Concatenating Data
+DECLARE @string1 NVARCHAR(100) = 'Chocolate with beans from';
+DECLARE @string2 NVARCHAR(100) = 'has a cocoa percentage of';
+
+SELECT
+	bean_type,
+	bean_origin,
+	cocoa_percent,
+	-- Create a message by concatenating values with "+"
+	@string1 + ' ' + bean_origin + ' ' + @string2 + ' ' + CAST(cocoa_percent AS nvarchar) AS message1,
+	-- Create a message by concatenating values with "CONCAT()"
+	CONCAT(@string1, ' ', bean_origin, ' ', @string2, ' ', cocoa_percent) AS message2,
+	-- Create a message by concatenating values with "CONCAT_WS()"
+	CONCAT_WS(' ', @string1, bean_origin, @string2, cocoa_percent) AS message3
+FROM ratings
+WHERE
+	company = 'Ambrosia'
+	AND bean_type <> 'Unknown';
+
+SELECT
+	company,
+    -- Create a list with all bean origins ordered alphabetically
+	STRING_AGG(bean_origin, ',') WITHIN GROUP (ORDER BY bean_origin) AS bean_origins
+FROM ratings
+WHERE company IN ('Bar Au Chocolat', 'Chocolate Con Amor', 'East Van Roasters')
+-- Specify the columns used for grouping your data
+GROUP BY company;
+
+-- company	bean_origins
+-- Bar Au Chocolat	Bahia,Chiapas,Duarte Province,Maranon Canyon,Sambirano
+-- Chocolate Con Amor	Dominican Republic,Dominican Republic,Ecuador,Nicaragua,Ocumare,Uganda,Wild Bolivian
+-- East Van Roasters	Dominican Republic,Madagascar,Peru
+
+DECLARE @phrase NVARCHAR(MAX) = 'In the morning I brush my teeth. In the afternoon I take a nap. In the evening I watch TV.'
+
+SELECT value
+FROM STRING_SPLIT(@phrase, '.');
+
+DECLARE @phrase NVARCHAR(MAX) = 'In the morning I brush my teeth. In the afternoon I take a nap. In the evening I watch TV.'
+
+SELECT value
+FROM STRING_SPLIT(@phrase, ' ');
+
+
+SELECT
+    -- Concatenate the first and last name
+	CONCAT('***' , first_name, ' ', UPPER(last_name), '***') AS name,
+    -- Mask the last two digits of the year
+    REPLACE(birthdate, SUBSTRING(CAST(birthdate AS varchar), 3, 2), 'XX') AS birthdate,
+	email,
+	country
+FROM voters
+   -- Select only voters with a first name less than 5 characters
+WHERE LEN(first_name) < 5
+   -- Look for this pattern in the email address: "j%[0-9]@yahoo.com"
+	AND PATINDEX('j_a%@yahoo.com', email) > 0;
+
+-- name	                birthdate	email	            country
+-- ***Juan ROMERO***	19XX-03-15	juan1@yahoo.com	    Germany
+-- ***Joan ROSS***	    19XX-06-22	joan8@yahoo.com	    Switzerland
+-- ***Joan MARTIN***	19XX-03-26	joan19@yahoo.com	Spain
