012181c5e262d6950163399de9e0b68c10e9ed62
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-08-06 11:44:06 +0200

    Added Transactions and Error Handling in SQL Server course script (lesson 2).

--- SQL/8. Transactions and Error Handling in SQL Server/Transactions_and_Error_Handling.sql
+++ SQL/8. Transactions and Error Handling in SQL Server/Transactions_and_Error_Handling.sql
@@ -82,3 +82,104 @@ BEGIN CATCH
 			ERROR_MESSAGE() AS message;
     END CATCH
 END CATCH
+
+-- LESSON 2
+-- CATCHING the RAISERROR
+BEGIN TRY
+    DECLARE @product_id INT = 5;
+    IF NOT EXISTS (SELECT * FROM products WHERE product_id = @product_id)
+        RAISERROR('No product with id %d.', 11, 1, @product_id);
+    ELSE
+        SELECT * FROM products WHERE product_id = @product_id;
+END TRY
+-- Catch the error
+BEGIN CATCH
+	-- Select the error message
+	SELECT ERROR_MESSAGE();
+END CATCH
+
+
+-- THROW
+CREATE PROCEDURE insert_product
+  @product_name VARCHAR(50),
+  @stock INT,
+  @price DECIMAL
+
+AS
+
+BEGIN TRY
+	INSERT INTO products (product_name, stock, price)
+		VALUES (@product_name, @stock, @price);
+END TRY
+-- Set up the CATCH block
+BEGIN CATCH
+	-- Insert the error and end the statement with a semicolon
+    INSERT INTO errors VALUES ('Error inserting a product');
+    -- Re-throw the error
+	THROW;
+END CATCH
+
+
+BEGIN TRY
+	-- Execute the stored procedure
+	EXEC insert_product
+    	-- Set the values for the parameters
+    	@product_name = 'Super bike',
+        @stock = 3,
+        @price = 499.99;
+END TRY
+-- Set up the CATCH block
+BEGIN CATCH
+	-- Select the error message
+	SELECT ERROR_MESSAGE();
+END CATCH
+
+-- Set @staff_id to 4
+DECLARE @staff_id INT = 4;
+
+IF NOT EXISTS (SELECT * FROM staff WHERE staff_id = @staff_id)
+   	-- Invoke the THROW statement with parameters
+	THROW 50001, 'No staff member with such id', 1;
+ELSE
+   	SELECT * FROM staff WHERE staff_id = @staff_id
+
+
+-- Set @first_name to 'Pedro'
+DECLARE @first_name NVARCHAR(20) = 'Pedro';
+-- Concat the message
+DECLARE @my_message NVARCHAR(500) =
+	FORMATMESSAGE('There is no staff member with ', @first_name, ' as the first name.');
+
+IF NOT EXISTS (SELECT * FROM staff WHERE first_name = @first_name)
+	-- Throw the error
+	THROW 50000, @my_message, 1;
+
+
+DECLARE @product_name AS NVARCHAR(50) = 'Trek CrossRip+ - 2018';
+DECLARE @number_of_sold_bikes AS INT = 10;
+DECLARE @current_stock INT;
+-- Select the current stock
+SELECT @current_stock = stock FROM products WHERE product_name = @product_name;
+DECLARE @my_message NVARCHAR(500) =
+	-- Customize the message
+	FORMATMESSAGE('There are not enough %s bikes. You only have %d in stock.', @product_name, @current_stock);
+
+IF (@current_stock - @number_of_sold_bikes < 0)
+	-- Throw the error
+	THROW 50000, @my_message, 1;
+
+
+-- Pass the variables to the stored procedure
+EXEC sp_addmessage @msgnum = 50002, @severity = 16, @msgtext = 'There are not enough %s bikes. You only have %d in stock.', @lang = N'us_english';
+
+DECLARE @product_name AS NVARCHAR(50) = 'Trek CrossRip+ - 2018';
+DECLARE @number_of_sold_bikes AS INT = 10;
+DECLARE @current_stock INT;
+SELECT @current_stock = stock FROM products WHERE product_name = @product_name;
+DECLARE @my_message NVARCHAR(500) =
+	-- Prepare the error message
+	FORMATMESSAGE(50002, @product_name, @current_stock);
+
+IF (@current_stock - @number_of_sold_bikes < 0)
+	-- Throw the error
+	THROW 50000, @my_message, 1;
