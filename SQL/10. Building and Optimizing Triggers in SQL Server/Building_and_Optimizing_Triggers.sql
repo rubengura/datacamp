24e69886a8fc5ef7e017c48e004eca98dfb0064c
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-08-08 13:34:05 +0200

    Added Building and Optimizing Triggers in SQL Server script (finished).

--- SQL/10. Building and Optimizing Triggers in SQL Server/Building_and_Optimizing_Triggers.sql
+++ SQL/10. Building and Optimizing Triggers in SQL Server/Building_and_Optimizing_Triggers.sql
@@ -321,3 +321,94 @@ FOR DROP_DATABASE
 AS
    PRINT 'You are not allowed to remove existing databases.';
    ROLLBACK;
+
+-- LESSON 4
+-- Trigger Optimization and Management
+-- Remove the trigger
+DROP TRIGGER PreventNewDiscounts;
+
+-- Remove the database trigger
+DROP TRIGGER PreventTableDeletion
+ON DATABASE;
+
+-- Remove the server trigger
+DROP TRIGGER DisallowLinkedServers
+ON ALL SERVER;
+
+-- Fix the typo in the trigger message
+ALTER TRIGGER PreventDiscountsDelete
+ON Discounts
+INSTEAD OF DELETE
+AS
+	PRINT 'You are not allowed to remove data from the Discounts table.';
+
+-- Pause the trigger execution
+DISABLE TRIGGER PreventOrdersUpdate
+ON Orders;
+
+-- Resume the trigger execution
+ENABLE TRIGGER PreventOrdersUpdate
+ON Orders;
+
+-- Get the disabled triggers
+SELECT name,
+	   object_id,
+	   parent_class_desc
+FROM sys.triggers
+WHERE is_disabled = 1;
+
+-- Check for unchanged server triggers
+SELECT *
+FROM sys.server_triggers
+WHERE create_date = modify_date;
+
+-- Get the database triggers
+SELECT *
+FROM sys.triggers
+WHERE parent_class_desc = 'DATABASE';
+
+-- Troubleshooting Triggers
+-- Modify the trigger to add new functionality
+ALTER TRIGGER PreventOrdersUpdate
+ON Orders
+-- Prevent any row changes
+INSTEAD OF UPDATE
+AS
+	-- Keep history of trigger executions
+	INSERT INTO TriggerAudit (TriggerName, ExecutionDate)
+	SELECT 'PreventOrdersUpdate',
+           GETDATE();
+
+	RAISERROR ('Updates on "Orders" table are not permitted.
+                Place a new order to add new products.', 16, 1);
+
+-- Get the table ID
+SELECT object_id AS TableID
+FROM sys.objects
+WHERE name = 'Orders';
+
+-- Get the trigger name
+SELECT t.name AS TriggerName
+FROM sys.objects AS o
+-- Join with the triggers table
+INNER JOIN sys.triggers AS t ON t.parent_id = o.object_id
+WHERE o.name = 'Orders';
+
+SELECT t.name AS TriggerName
+FROM sys.objects AS o
+INNER JOIN sys.triggers AS t ON t.parent_id = o.object_id
+-- Get the trigger events
+INNER JOIN sys.trigger_events AS te ON te.object_id = t.object_id
+WHERE o.name = 'Orders'
+-- Filter for triggers reacting to new rows
+AND te.type_desc = 'UPDATE';
+
+
+SELECT t.name AS TriggerName,
+	   OBJECT_DEFINITION(t.object_id) AS TriggerDefinition
+FROM sys.objects AS o
+INNER JOIN sys.triggers AS t ON t.parent_id = o.object_id
+INNER JOIN sys.trigger_events AS te ON te.object_id = t.object_id
+WHERE o.name = 'Orders'
+AND te.type_desc = 'UPDATE';
+
