3972ad2a7cf0c6648ee25cdf54b8693bba27ffd0
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-08-08 20:57:44 +0200

    Added Improving Query Performance in SQL Server script (finished).

--- SQL/11. Improving Query Performance in SQL Server/Improving_Query_Performance.sql
+++ SQL/11. Improving Query Performance in SQL Server/Improving_Query_Performance.sql
@@ -405,3 +405,93 @@ LEFT OUTER JOIN Orders o
 WHERE c.Country = 'France'
 	AND o.CustomerID IS NULL; -- Filter condition
 
+
+-- LESSON 4
+-- Time Statistics
+SET STATISTICS TIME ON -- Turn the time command on
+
+-- Query 1
+SELECT *
+FROM Teams
+-- Sub-query 1
+WHERE City IN -- Sub-query filter operator
+      (SELECT CityName
+       FROM Cities) -- Table from Earthquakes database
+-- Sub-query 2
+   AND City IN -- Sub-query filter operator
+	   (SELECT CityName
+	    FROM Cities
+		WHERE CountryCode IN ('US','CA'))
+-- Sub-query 3
+    AND City IN -- Sub-query filter operator
+        (SELECT CityName
+         FROM Cities
+	     WHERE Pop2017 >2000000);
+
+-- Query 2
+SELECT *
+FROM Teams AS t
+WHERE EXISTS -- Sub-query filter operator
+	(SELECT 1
+     FROM Cities AS c
+     WHERE t.City = c.CityName -- Columns being compared
+        AND c.CountryCode IN ('US','CA')
+          AND c.Pop2017 > 2000000);
+
+SET STATISTICS TIME OFF -- Turn the time command off
+
+-- STATISTICS IO
+SET STATISTICS IO ON -- Turn the IO command on
+
+-- Example 1
+SELECT CustomerID,
+       CompanyName,
+       (SELECT COUNT(*)
+	    FROM Orders AS o -- Add table
+		WHERE c.CustomerID = o.CustomerID) CountOrders
+FROM Customers AS c
+WHERE CustomerID IN -- Add filter operator
+       (SELECT CustomerID
+	    FROM Orders
+		WHERE ShipCity IN
+            ('Berlin','Bern','Bruxelles','Helsinki',
+			'Lisboa','Madrid','Paris','London'));
+
+-- Example 2
+SELECT c.CustomerID,
+       c.CompanyName,
+       COUNT(o.CustomerID)
+FROM Customers AS c
+LEFT JOIN Orders AS o -- Join operator
+    ON c.CustomerID = o.CustomerID
+WHERE o.ShipCity IN -- Shipping destination column
+     ('Berlin','Bern','Bruxelles','Helsinki',
+	 'Lisboa','Madrid','Paris','London')
+GROUP BY c.CustomerID,
+         c.CompanyName;
+
+SET STATISTICS IO OFF -- Turn the IO command off
+
+-- Indexes
+-- Query 1
+SELECT *
+FROM Cities
+WHERE CountryCode = 'RU' -- Country code
+		OR CountryCode = 'CN' -- Country code
+
+-- Query 2 (After putting an index on city the query runs much faster)
+SELECT *
+FROM Cities
+WHERE CountryCode IN ('JA','NZ') -- Country codes
+
+
+-- Execution plans
+SELECT CityName AS NearCityName,
+	   CountryCode
+FROM Cities
+
+UNION -- Append queries
+
+SELECT Capital AS NearCityName,
+       Code2 AS CountryCode
+FROM Nations;
