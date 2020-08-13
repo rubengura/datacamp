f764447c59bd3b389641b32a8c288565a62319b4
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-08-13 09:31:45 +0200

    Added Introduction to SQL Server course script (lesson 2).

--- SQL/Data Analyst Track/1. Introduction to SQL Server/Introduction_to_SQL_Server.sql
+++ SQL/Data Analyst Track/1. Introduction to SQL Server/Introduction_to_SQL_Server.sql
@@ -151,3 +151,119 @@ ORDER BY
   release_year,
   artist,
   song;
+
+-- LESSON 2
+-- Sum the demand_loss_mw column
+SELECT
+  SUM(demand_loss_mw) AS MRO_demand_loss
+FROM
+  grid
+WHERE
+  -- demand_loss_mw should not contain NULL values
+  demand_loss_mw IS NOT NULL
+  -- and nerc_region should be 'MRO';
+  AND nerc_region = 'MRO';
+
+-- Obtain a count of 'grid_id'
+SELECT
+  COUNT(grid_id) AS RFC_count
+FROM
+  grid
+-- Restrict to rows where the nerc_region is 'RFC'
+WHERE
+  nerc_region = 'RFC';
+
+
+-- Find the minimum number of affected customers
+SELECT
+  MIN(affected_customers) AS min_affected_customers,
+  MAX(affected_customers) AS max_affected_customers,
+  AVG(affected_customers) AS avg_affected_customers
+FROM
+  grid
+-- Only retrieve rows where demand_loss_mw has a value
+WHERE
+  demand_loss_mw IS NOT NULL;
+
+
+-- Calculate the length of the description column
+SELECT
+  LEN (description) AS description_length
+FROM
+  grid;
+
+-- Select the first 25 characters from the left of the description column
+SELECT
+  LEFT(description, 25) AS first_25_left,
+  RIGHT(description, 25) AS last_25_right
+FROM
+  grid;
+
+-- Complete the substring function to begin extracting from the correct character in the description column
+SELECT TOP (10)
+  description,
+  CHARINDEX('Weather', description) AS start_of_string,
+  LEN ('Weather') AS length_of_string,
+  SUBSTRING(
+    description,
+    15,
+    LEN(description)
+  ) AS additional_description
+FROM
+  grid
+WHERE description LIKE '%Weather%';
+
+-- Select the region column
+SELECT
+  nerc_region,
+  -- Sum the demand_loss_mw column
+  SUM(demand_loss_mw) AS demand_loss
+FROM
+  grid
+  -- Exclude NULL values of demand_loss
+WHERE
+  demand_loss_mw IS NOT NULL
+  -- Group the results by nerc_region
+GROUP BY
+  nerc_region
+  -- Order the results in descending order of demand_loss
+ORDER BY
+  demand_loss DESC;
+
+
+SELECT
+  nerc_region,
+  SUM (demand_loss_mw) AS demand_loss
+FROM
+  grid
+  -- Remove the WHERE clause
+WHERE demand_loss_mw  IS NOT NULL
+GROUP BY
+  nerc_region
+  -- Enter a new HAVING clause so that the sum of demand_loss_mw is greater than 10000
+HAVING
+  SUM(demand_loss_mw) > 10000
+ORDER BY
+  demand_loss DESC;
+
+
+
+SELECT
+  country,
+  COUNT (country) AS country_count,
+  AVG (place) AS avg_place,
+  AVG (points) AS avg_points,
+  MIN (points) AS min_points,
+  MAX (points) AS max_points
+FROM
+  eurovision
+GROUP BY
+  country
+  -- The country column should only contain those with a count greater than 5
+HAVING
+  COUNT(country) > 5
+  -- Arrange columns in the correct order
+ORDER BY
+  avg_place,
+  avg_points DESC;
+
