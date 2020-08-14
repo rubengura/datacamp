8df73adc1fe6db8d4e38819898ca81d477c8d66d
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-08-14 11:10:52 +0200

    Added Introduction to Relational Databases in SQL Server course script (finished).

--- SQL/Data Analyst Track/2. Introduction to Relational Databases in SQL Server/Introduction_to_Relational_Databases.sql
+++ SQL/Data Analyst Track/2. Introduction to Relational Databases in SQL Server/Introduction_to_Relational_Databases.sql
@@ -125,3 +125,199 @@ ADD CONSTRAINT university_shortname_unq UNIQUE(university_shortname);
 -- Make organizations.organization unique
 ALTER TABLE organizations
 ADD CONSTRAINT organization_unq UNIQUE(organization);
+
+
+-- LESSON 3
+
+-- Count the number of rows in universities
+SELECT COUNT(DISTINCT(university_shortname))
+FROM universities;
+
+-- Count the number of distinct values in the university_city column
+SELECT COUNT(DISTINCT(university_city))
+FROM universities;
+
+
+-- Try out different combinations
+SELECT COUNT(DISTINCT(firstname, lastname))
+FROM professors;
+
+-- Rename the organization column to id
+ALTER TABLE organizations
+RENAME COLUMN organization TO id;
+
+-- Make id a primary key
+ALTER TABLE organizations
+ADD CONSTRAINT organization_pk PRIMARY KEY (id);
+
+-- Add the new column to the table
+ALTER TABLE professors
+ADD COLUMN id serial;
+
+-- Add the new column to the table
+ALTER TABLE professors
+ADD COLUMN id serial;
+
+-- Make id a primary key
+ALTER TABLE professors
+ADD CONSTRAINT professors_pkey PRIMARY KEY (id);
+
+-- Add the new column to the table
+ALTER TABLE professors
+ADD COLUMN id serial;
+
+-- Make id a primary key
+ALTER TABLE professors
+ADD CONSTRAINT professors_pkey PRIMARY KEY (id);
+
+-- Have a look at the first 10 rows of professors
+SELECT * FROM professors LIMIT 10;
+
+-- Count the number of distinct rows with columns make, model
+SELECT COUNT(DISTINCT(make, model))
+FROM cars;
+
+-- Add the id column
+ALTER TABLE cars
+ADD COLUMN id varchar(128);
+
+-- Update id with make + model
+UPDATE cars
+SET id = CONCAT(make, model);
+
+-- Make id a primary key
+ALTER TABLE cars
+ADD CONSTRAINT id_pk PRIMARY KEY(id);
+
+-- Have a look at the table
+SELECT * FROM cars;
+
+-- Create the table
+CREATE TABLE students (
+  last_name varchar(128) NOT NULL,
+  ssn integer PRIMARY KEY,
+  phone_no char(12)
+);
+
+
+-- LESSON 4
+-- Rename the university_shortname column
+ALTER TABLE professors
+RENAME COLUMN university_shortname TO university_id;
+
+-- Add a foreign key on professors referencing universities
+ALTER TABLE professors
+ADD CONSTRAINT professors_fkey FOREIGN KEY (university_id) REFERENCES universities (id);
+
+
+-- Try to insert a new professor
+INSERT INTO professors (firstname, lastname, university_id)
+VALUES ('Albert', 'Einstein', 'UZH');
+
+-- Select all professors working for universities in the city of Zurich
+SELECT professors.lastname, universities.id, universities.university_city
+FROM professors
+JOIN universities
+ON professors.university_id = universities.id
+WHERE universities.university_city = 'Zurich';
+
+-- Add a professor_id column
+ALTER TABLE affiliations
+ADD COLUMN professor_id integer REFERENCES professors (id);
+
+-- Rename the organization column to organization_id
+ALTER TABLE affiliations
+RENAME organization TO organization_id;
+
+-- Add a foreign key on organization_id
+ALTER TABLE affiliations
+ADD CONSTRAINT affiliations_organization_fkey FOREIGN KEY (organization_id) REFERENCES organizations (id);
+
+-- Have a look at the 10 first rows of affiliations
+SELECT * FROM affiliations LIMIT 10;
+
+-- Set professor_id to professors.id where firstname, lastname correspond to rows in professors
+UPDATE affiliations
+SET professor_id = professors.id
+FROM professors
+WHERE affiliations.firstname = professors.firstname AND affiliations.lastname = professors.lastname;
+
+-- Have a look at the 10 first rows of affiliations again
+SELECT * FROM affiliations LIMIT 10;
+
+-- Drop the firstname column
+ALTER TABLE affiliations
+DROP COLUMN firstname;
+
+-- Drop the lastname column
+ALTER TABLE affiliations
+DROP COLUMN lastname;
+
+-- Identify the correct constraint name
+SELECT constraint_name, table_name, constraint_type
+FROM information_schema.table_constraints
+WHERE constraint_type = 'FOREIGN KEY';
+
+-- Drop the right foreign key constraint
+ALTER TABLE affiliations
+DROP CONSTRAINT affiliations_organization_id_fkey;
+
+-- Add a new foreign key constraint from affiliations to organizations which cascades deletion
+ALTER TABLE affiliations
+ADD CONSTRAINT affiliations_organization_id_fkey FOREIGN KEY (organization_id) REFERENCES organizations (id) ON DELETE CASCADE;
+
+-- Delete an organization
+DELETE FROM organizations
+WHERE id = 'CUREM';
+
+-- Check that no more affiliations with this organization exist
+SELECT * FROM organizations
+WHERE id = 'CUREM';
+
+-- Count the total number of affiliations per university
+SELECT COUNT(*), professors.university_id
+FROM affiliations
+JOIN professors
+ON affiliations.professor_id = professors.id
+GROUP BY professors.university_id
+ORDER BY count DESC;
+
+
+-- Join all tables
+SELECT *
+FROM affiliations
+JOIN professors
+ON affiliations.professor_id = professors.id
+JOIN organizations
+ON affiliations.organization_id = organizations.id
+JOIN universities
+ON professors.university_id = universities.id;
+
+-- Group the table by organization sector, professor and university city
+SELECT COUNT(*), organizations.organization_sector,
+professors.id, universities.university_city
+FROM affiliations
+JOIN professors
+ON affiliations.professor_id = professors.id
+JOIN organizations
+ON affiliations.organization_id = organizations.id
+JOIN universities
+ON professors.university_id = universities.id
+GROUP BY organizations.organization_sector,
+professors.id, universities.university_city;
+
+-- Filter the table and sort it
+SELECT COUNT(*), organizations.organization_sector,
+professors.id, universities.university_city
+FROM affiliations
+JOIN professors
+ON affiliations.professor_id = professors.id
+JOIN organizations
+ON affiliations.organization_id = organizations.id
+JOIN universities
+ON professors.university_id = universities.id
+WHERE organizations.organization_sector = 'Media & communication'
+GROUP BY organizations.organization_sector,
+professors.id, universities.university_city
+ORDER BY count DESC;
+
