ef8fe39820c900bf636ae0dd110da3b88a216372
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-08-14 09:37:22 +0200

    Added Introduction to SQL Server course script (finished).

--- SQL/Data Analyst Track/1. Introduction to SQL Server/Introduction_to_SQL_Server.sql
+++ SQL/Data Analyst Track/1. Introduction to SQL Server/Introduction_to_SQL_Server.sql
@@ -268,6 +268,9 @@ ORDER BY
   avg_points DESC;
 
 
+
+-- LESSON 3
+
 SELECT
   track_id,
   name AS track_name,
@@ -334,3 +337,191 @@ SELECT
   'Artist'  AS Source
   -- Complete the FROM statement
 FROM artist;
+
+
+SELECT
+  track_id,
+  name AS track_name,
+  title AS album_title
+FROM track
+  -- Complete the join type and the common joining column
+INNER JOIN album on album.album_id = track.album_id;
+
+-- Select album_id and title from album, and name from artist
+SELECT
+  album_id,
+  title,
+  name AS artist
+  -- Enter the main source table name
+FROM album
+  -- Perform the inner join
+INNER JOIN artist on album.artist_id = artist.artist_id;
+
+SELECT track_id,
+-- Enter the correct table name prefix when retrieving the name column from the track table
+  track.name AS track_name,
+  title as album_title,
+  -- Enter the correct table name prefix when retrieving the name column from the artist table
+  artist.name AS artist_name
+FROM track
+  -- Complete the matching columns to join album with track, and artist with album
+INNER JOIN album on album.album_id = track.album_id
+INNER JOIN artist on artist.artist_id = album.artist_id;
+
+SELECT
+  invoiceline_id,
+  unit_price,
+  quantity,
+  billing_state
+  -- Specify the source table
+FROM invoiceline
+  -- Complete the join to the invoice table
+LEFT JOIN invoice
+ON invoiceline.invoice_id = invoice.invoice_id;
+
+-- SELECT the fully qualified album_id column from the album table
+SELECT
+  album_id,
+  title,
+  album.artist_id,
+  -- SELECT the fully qualified name column from the artist table
+  name as artist
+FROM album
+-- Perform a join to return only rows that match from both tables
+INNER JOIN artist ON album.artist_id = artist.artist_id
+WHERE album.album_id IN (213,214)
+
+SELECT
+  album_id AS ID,
+  title AS description,
+  'Album' AS Source
+  -- Complete the FROM statement
+FROM album
+ -- Combine the result set using the relevant keyword
+UNION
+SELECT
+  artist_id AS ID,
+  name AS description,
+  'Artist'  AS Source
+  -- Complete the FROM statement
+FROM artist;
+
+
+-- LESSON 4
+-- Create the table
+CREATE TABLE results (
+	-- Create track column
+	track VARCHAR(200),
+    -- Create artist column
+	artist VARCHAR(120),
+    -- Create album column
+	album VARCHAR(160),
+	);
+
+-- Create the table
+CREATE TABLE tracks(
+  -- Create track column
+  track VARCHAR(200),
+  -- Create album column
+  album VARCHAR(160),
+  -- Create track_length_mins column
+  track_length_mins INT
+);
+-- Complete the statement to enter the data to the table
+INSERT INTO tracks
+-- Specify the destination columns
+(track, album, track_length_mins)
+-- Insert the appropriate values for track, album and track length
+VALUES
+  ('Basket Case', 'Dookie', 3);
+-- Select all columns from the new table
+SELECT
+  *
+FROM
+  tracks;
+
+-- Select the album
+SELECT
+  title
+FROM
+  album
+WHERE
+  album_id = 213;
+-- UPDATE the title of the album
+UPDATE
+  album
+SET
+  title = 'Pure Cult: The Best Of The Cult'
+WHERE
+  album_id = 213;
+-- Run the query again
+SELECT
+  title
+FROM
+  album ;
+
+-- Run the query
+SELECT
+  *
+FROM
+  album
+  -- DELETE the record
+DELETE FROM
+  album
+WHERE
+  album_id = 1
+  -- Run the query again
+SELECT
+  *
+FROM
+  album;
+
+-- Declare the variable @region
+DECLARE @region VARCHAR(10)
+
+-- Update the variable value
+SET @region = 'RFC'
+
+SELECT description,
+       nerc_region,
+       demand_loss_mw,
+       affected_customers
+FROM grid
+WHERE nerc_region = @region;
+
+-- Declare your variables
+DECLARE @start DATE
+DECLARE @stop DATE
+DECLARE @affected INT;
+-- SET the relevant values for each variable
+SET @start = '2014-01-24'
+SET @stop  = '2014-07-02'
+SET @affected =  5000 ;
+
+SELECT
+  description,
+  nerc_region,
+  demand_loss_mw,
+  affected_customers
+FROM
+  grid
+-- Specify the date range of the event_date and the value for @affected
+WHERE event_date BETWEEN @start AND @stop
+AND affected_customers >= @affected;
+
+
+SELECT  album.title AS album_title,
+  artist.name as artist,
+  MAX(track.milliseconds / (1000 * 60) % 60 ) AS max_track_length_mins
+-- Name the temp table #maxtracks
+INTO #maxtracks
+FROM album
+-- Join album to artist using artist_id
+INNER JOIN artist ON album.artist_id = artist.artist_id
+-- Join track to album using album_id
+INNER JOIN track ON album.album_id = track.album_id
+GROUP BY artist.artist_id, album.title, artist.name,album.album_id
+-- Run the final SELECT query to retrieve the results from the temporary table
+SELECT album_title, artist, max_track_length_mins
+FROM  #maxtracks -- Temporary table
+ORDER BY max_track_length_mins DESC, artist;
