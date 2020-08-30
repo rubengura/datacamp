139b586fa98f2e93dbfd714e5b78826af02e0173
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-08-30 20:06:25 +0200

    Added Introduction to R course (finished).

--- R/Data Science Track/Introduction to R.R
+++ R/Data Science Track/Introduction to R.R
@@ -221,3 +221,98 @@ da5 <- factor_speed_vector[5]
 # Is data analyst 2 faster than data analyst 5?
 da2 > da5
 # FALSE
+
+
+# LESSON 5 Data frames
+# Call head() on mtcars
+head(mtcars)
+
+# Investigate the structure of mtcars
+str(mtcars)
+
+
+# Definition of vectors
+name <- c("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+type <- c("Terrestrial planet", "Terrestrial planet", "Terrestrial planet",
+          "Terrestrial planet", "Gas giant", "Gas giant", "Gas giant", "Gas giant")
+diameter <- c(0.382, 0.949, 1, 0.532, 11.209, 9.449, 4.007, 3.883)
+rotation <- c(58.64, -243.02, 1, 1.03, 0.41, 0.43, -0.72, 0.67)
+rings <- c(FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE)
+
+# Create a data frame from the vectors
+planets_df <- data.frame(name, type, diameter, rotation, rings)
+
+
+# Print out diameter of Mercury (row 1, column 3)
+planets_df[1,3]
+
+# Print out data for Mars (entire fourth row)
+planets_df[4,]
+
+# Select first 5 values of diameter column
+planets_df[1:5,"diameter"]
+
+# Select the rings variable from planets_df
+rings_vector <- planets_df$rings
+
+# Print out rings_vector
+rings_vector
+
+# Adapt the code to select all columns for planets with rings
+planets_df[rings_vector,]
+
+# Select planets with diameter < 1
+subset(planets_df, diameter < 1)
+
+# Play around with the order function in the console
+a <- c(100, 10, 1000)
+order(a)
+a[order(a)]
+
+# Use order() to create positions
+positions <-  order(planets_df$diameter)
+
+# Use positions to sort planets_df
+planets_df[positions,]
+
+# LESSON 6 Lists
+# Vector with numerics from 1 up to 10
+my_vector <- 1:10
+
+# Matrix with numerics from 1 up to 9
+my_matrix <- matrix(1:9, ncol = 3)
+
+# First 10 elements of the built-in data frame mtcars
+my_df <- mtcars[1:10,]
+
+# Construct list with these different elements:
+my_list <- list(my_vector, my_matrix, my_df)
+
+# Adapt list() call to give the components names
+my_list <- list(vec = my_vector, mat = my_matrix, df = my_df)
+
+# Print out my_list
+my_list
+
+# Finish the code to build shining_list
+shining_list <- list(moviename = mov, actors = act, reviews = rev)
+
+# Print out the vector representing the actors
+shining_list$actors
+
+# Print the second element of the vector representing the actors
+shining_list$actors[2]
+
+# Use the table from the exercise to define the comments and scores vectors
+scores <- c(4.6, 5, 4.8, 5, 4.2)
+comments <- c("I would watch it again", "Amazing!", "I liked it", "One of the best movies", "Fascinating plot")
+# Save the average of the scores vector as avg_review
+avg_review <- mean(scores)
+
+# Combine scores and comments into the reviews_df data frame
+reviews_df <- data.frame(scores, comments)
+
+# Create and print out a list, called departed_list
+departed_list <- list(movie_title, movie_actors, reviews_df, avg_review)
+
+departed_list
